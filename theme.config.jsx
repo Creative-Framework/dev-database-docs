1cd0258c443a96c20c2d7f1fe22fa630e4fa4ab4
Shimizu Hiroyuki <amtixdev@gmail.com>
2026-05-20 02:02:19 +0300

    Add theme.config.jsx: core implementation and docs

---
+++ theme.config.jsx
@@ -0,0 +1,15 @@
+export default {
+  logo: <strong>Dev-Database Docs</strong>,
+  project: {
+    link: 'https://github.com/Creative-Framework/dev-database'
+  },
+  docsRepositoryBase: 'https://github.com/Creative-Framework/dev-database/tree/main/docs',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Dev-Database'
+    }
+  },
+  footer: {
+    text: 'MIT 2026 © Creative-Framework.'
+  }
+}
